427045136a04c7e2378548ce7525d2f3708369dc
Bruno Florêncio Alves <bruno.f7@hotmail.com>
2022-03-07 00:34:41 -0300

    :up: jquery and style

--- jquery.js
+++ jquery.js
@@ -1,17 +1,17 @@
 $(".st1").click(function (){
-    $(".profile").prop("checked", true);
+    $(".admin").prop("checked", true);
 })
 $(".st2").click(function (){
-    $(".settings").prop("checked", true);
+    $(".projetos").prop("checked", true);
 })
 $(".st3").click(function (){
-    $(".post").prop("checked", true);
+    $(".auditCont").prop("checked", true);
 })
 $(".st4").click(function (){
-    $(".books").prop("checked", true);
+    $(".compliance").prop("checked", true);
 })
 $(".st5").click(function (){
-    $(".books").prop("checked", true);
+    $(".sistemas").prop("checked", true);
 })
 $("ul li").click(function(){
     $(this).addClass("active").siblings().romoveClass("active");
